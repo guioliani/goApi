fb020a32d71927f4b2ce8514908a0006e599daa0
Guilherme oliani <guilhermeagutoli@gmail.com>
2024-04-12 22:36:33 -0300

    adicionado a classe de campanha

---
+++ internal/domain/campaign/campaign_test.go
@@ -0,0 +1,46 @@
+package campaign
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	name     = "Campaign X"
+	content  = "Body"
+	contacts = []string{"email1@gmail.com", "email2@gmail.com"}
+)
+
+func TestShouldCreateNewCampaign(t *testing.T) {
+	assert := assert.New(t)
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func TestNewCampaignIdIsNotNill(t *testing.T) {
+	assert := assert.New(t)
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.Id)
+}
+
+func TestNewCampaignCreatedOnisNotNill(t *testing.T) {
+	assert := assert.New(t)
+	now := time.Now().Add(-time.Minute)
+
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.Greater(campaign.CreatedOn, now)
+}
+
+func TestNewCampaignShouldNotCreateNameNil(t *testing.T) {
+	assert := assert.New(t)
+	_, err := NewCampaign("", content, contacts)
+
+	assert.Equal("name is required", err.Error())
+}
