f7d7c5bfa8781db4f87b663acca5532c4cb0dd84
Guilherme oliani <guilhermeagutoli@gmail.com>
2024-07-18 17:25:38 -0300

    adicionado a pasta endpoints separando as chamadas do arquivo main, e adicionado a parte de infraStructure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"goapi/internal/domain/campaign"
+	"goapi/internal/endpoints"
+	databse "goapi/internal/infraStructure/database"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -9,5 +13,17 @@ import (
 func main() {
 	r := chi.NewRouter()
 
+	campaignService := campaign.Service{
+		Repository: &databse.CampaignRepository{},
+	}
+
+	handler := endpoints.Handler{
+		CampaignService: campaignService,
+	}
+
+	//routes
+	r.Post("/campaigns", handler.CreateCampaign)
+	r.Get("/campaigns", handler.GetCampaign)
+	log.Println("Server starting on port 3000")
 	http.ListenAndServe(":3000", r)
 }
